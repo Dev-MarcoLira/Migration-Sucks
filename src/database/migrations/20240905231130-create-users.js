41483ea07d890f309d7eb4c2d3d21cf931928ded
Marco Antonio <malirab2302@gmail.com>
2024-09-05 20:13:06 -0300

    add migration

---
+++ src/database/migrations/20240905231130-create-users.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      
+      return queryInterface.createTable('users', 
+        { id: Sequelize.INTEGER }
+      );
+    
+  },
+
+  down: (queryInterface, Sequelize) => {
+
+    return queryInterface.dropTable('users');
+
+  }
+};
